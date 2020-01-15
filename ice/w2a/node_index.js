ac3888a8c4d25dec403fb3d8a5c03a6b9aa793f9
Taylor Thompson <ta688070@ucf.edu>
2020-01-15 12:06:22 -0500

    #57 In class demo

--- ice/w2a/node_index.js
+++ ice/w2a/node_index.js
@@ -1,6 +1,10 @@
-function scopeDemo() {
-    console.log(x)
+console.log("Hello Worlds!");
+var x = [1];
+function scopeDemo(x1) {
+    console.log(x1);
+    console.log(x);
     let a = 3, b = 4, c = 5;
+    x1[0] = 500;
     console.log(a);
     console.log(b);
     console.log(c);
@@ -9,3 +13,5 @@ function scopeDemo() {
         var x = 0;
     }
 }
+scopeDemo(x);
+console.log(x);
