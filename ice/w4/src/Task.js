43c1d61104524dbf1848b7179e7a8282d91533f3
Taylor Thompson <ta688070@ucf.edu>
2020-01-29 12:15:44 -0500

    Co-authored-by: y604499781 <54754052+y604499781@users.noreply.github.com>

---
+++ ice/w4/src/Task.js
@@ -0,0 +1,17 @@
+class Task
+{
+    constructor(props)
+    {
+        this.props = props;
+    }
+
+    render()
+    {
+        return `<span>
+        <input type="checkbox"></input>
+        <span>${this.props.content}</span>
+    </span>`;
+    }
+}
+
+export default Task;
--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -2,7 +2,7 @@ import Component from "./Component.js";
 import Task from "./Task.js";
 function runOnLoad()
 {
-    
+    console.log("New change");
     let element = document.createElement("div");
     element.id = "Test";
     document.body.appendChild(element);
@@ -11,6 +11,17 @@ function runOnLoad()
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
     console.log("Hello World truly");
+    let myTask = new Task(
+        {
+            content: "Finish ICE Exercise",
+            done: false
+        }
+    )
+    var container = document.getElementById("taskList");
+    var newTask = document.createElement("div");
+    newTask.innerHTML = myTask.render();
+    container.appendChild(newTask);
+    console.log(myTask.render());
 }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
