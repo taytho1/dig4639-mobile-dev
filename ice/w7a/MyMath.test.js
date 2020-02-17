e0ca608890f2bdb638b5c3034faf713870c1430b
Taylor Thompson <ta688070@ucf.edu>
2020-02-17 12:26:39 -0500

    Jest

--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,7 +1,71 @@
-import { Sum } from './MyMath.js'
+import { Sum, 
+  AddList, 
+  DivideBy, 
+  ContainsString, 
+  ReSortedNumbers, 
+  Adder  
+} from './MyMath.js'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
+//Sum
+describe("Sum", () => {
+
+  test('Whether undefined is returned on individual type', () => {
+    expect(Sum(1, "Test")).toBeUndefined()
+  })
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(Sum(1, 2)).toBe(3)
+  })
+
+  it('produces h sum of 10 + 20, which should be 30', () => {
+    expect(Sum(10,20)).toBe(30)
+  })
+})
+
+//AddList
+describe("AddList", () => {
+
+  test('Whether undefined is returned on individual type', () => {
+    let numElem = 1000;
+    let input = [];
+    let result = 0;
+    var elem = 0;
+    for (var j=0;j<10;j++){
+      for(var i=0;i<numElem; i++){
+        elem = Math.random()*1000;
+        result+= elem;
+        input.push(elem);
+      }
+      expect(AddList(input)).toBe(result);
+    }
+  })
+  test('Tests if empty array returns undefined', () => {
+    let input = [];
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('Tests if undefined element returns undefined', () => {
+    let input = [undefined,0,2,3];
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('Tests if non-array returns undefined', () => {
+    let input = 100;
+    expect(AddList(input)).toBeUndefined();
+  })
+})
+
+//DivideBy
+describe("DivideBy", () => {
+  test('Whether undefined is returned on individual type', () => {
+    expect(DivideBy(1, "Test")).toBeUndefined();
+  })
+
+  test('b is 0', () => {
+    expect(DivideBy(1, 0)).toBeUndefined();
+  })
+
+  test('a/b', () => {
+    expect(DivideBy(20,10)).toBe(2)
+  })
 })
 
 // ICE 
