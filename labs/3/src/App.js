a560fbf688ede94c2825de89f381befe4fd549b8
Taylor <thompson.taylor97@knights.ucf.edu>
2020-02-21 18:22:30 -0500

    Lab 3

--- labs/3/src/App.js
+++ labs/3/src/App.js
@@ -5,9 +5,10 @@ import Card from './components/Card/index.js';
 class App extends React.Component {
   render(){
     return(
+      
       <Card content="This is a card!"></Card>
+      
     );
   }
 }
-
 export default App;
--- labs/3/src/components/Card/index.js
+++ labs/3/src/components/Card/index.js
@@ -3,17 +3,12 @@ import "./index.css";
 
 class Card extends React.Component
 {
-
-    constructor(props)
-    {
-        super(props);
-        this.element = document.createElement("div");
-        this.element.className = "card";
-        this.element = this.props.content;
-    }
-//{
-  //  return <div className="card"></div>
-//}
+  render()
+  {
+    const element = this.props.content;
+    return (
+    <div className="card">{element}</div>
+    );
+  }
 }
-render(<Card />, this.element);
 export default Card;
